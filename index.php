a03cce59366dfa17a958ef3cad4c4cca5624baa9
Renato Marques <renato@buildbox.com.br>
2025-04-17 01:42:44 -0300

    🔨 home new sections

--- index.php
+++ index.php
@@ -5,8 +5,8 @@
             <div class="row">
                 <div class="col-md-4">
                     <div class="box-hero">
-                        <h2>affitta sucuro a costo zero</h2>
-                        <p>risparmia tempo e trova oggi la casa per te</p>
+                        <h2>Mais de 400 MEI,s</h2>
+                        <p>profissionais de alto nível a sua disposição!</p>
                     </div>
                 </div>
                 <div class="col-md-8">
@@ -20,48 +20,81 @@
             </div>
         </div>
     </section>
-    <section class="vantaggi">
 
+    <section class="profissoes">
         <div class="container">
             <div class="row">
                 <div class="col-12">
-                    <h2>VANTAGGI</h2>
+                    <h2>PROFISSÕES POPULARES</h2>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12">
+                    <div class="profissoes-list">
+                        <a href="#" class="profissao-item">Eletricista</a>
+                        <a href="#" class="profissao-item">Pedreiro</a>
+                        <a href="#" class="profissao-item">Encanador</a>
+                        <a href="#" class="profissao-item">Jardineiro</a>
+                        <a href="#" class="profissao-item">Doméstico</a>
+                        <a href="#" class="profissao-item">Azulejista</a>
+                        <a href="#" class="profissao-item">Mecânico</a>
+                        <a href="#" class="profissao-item">Chapeador</a>
+                        <a href="#" class="profissao-item">Borracheiro</a>
+                        <a href="#" class="profissao-item">Vidraceiro</a>
+                        <a href="#" class="profissao-item">Metalúrgico</a>
+                        <a href="#" class="profissao-item">Músico</a>
+                    </div>
+                    <p class="text-center mt-4">
+                        <strong>
+                            O BuscaMEI conecta você a profissionais MEI que oferecem os serviços que você precisa,
+                            ajudando a encontrar o profissional ideal para o trabalho.
+                        </strong>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="vantaggi">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <h2>FORMALIZE-SE COMO MEI</h2>
                 </div>
             </div>
             <div class="row">
                 <div class="col-3">
                     <div class="box-vantaggi">
-                        <img src="images/icon-pesquisar.png" alt="">
-                        <p class="title">trova</p>
-                        <p class="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean consectetur massa at commodo bibendum. Aliquam porta ornare purus vel posuere. Morbi porta sem quis tempor pretium. </p>
-
+                        <img src="images/icon-cnpj.svg" alt="CNPJ Ativo">
+                        <p class="title">CNPJ Ativo</p>
+                        <p class="text">Desfrute da cidadania empresarial e ganhe mais credibilidade no mercado, podendo emitir notas fiscais e ter acesso a diversos benefícios.</p>
                     </div>
                 </div>
                 <div class="col-3">
                     <div class="box-vantaggi">
-                        <img src="images/icon-agendar.png" alt="">
-                        <p class="title">prenota online</p>
-                        <p class="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean consectetur massa at commodo bibendum. Aliquam porta ornare purus vel posuere. Morbi porta sem quis tempor pretium. </p>
+                        <img src="images/icon-nota.svg" alt="Nota Fiscal">
+                        <p class="title">Nota Fiscal</p>
+                        <p class="text">Regularize suas operações e ofereça segurança aos seus clientes, aumentando a confiança em seus serviços e reduzindo a informalidade.</p>
                     </div>
                 </div>
                 <div class="col-3">
                     <div class="box-vantaggi">
-                        <img src="images/icon-visitar.png" alt="">
-                        <p class="title">visita</p>
-                        <p class="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean consectetur massa at commodo bibendum. Aliquam porta ornare purus vel posuere. Morbi porta sem quis tempor pretium. </p>
+                        <img src="images/icon-inss.svg" alt="INSS">
+                        <p class="title">INSS Garantido</p>
+                        <p class="text">Garanta sua contribuição previdenciária com todos os benefícios assegurados pela legislação: aposentadoria, auxílio-doença e licença-maternidade.</p>
                     </div>
                 </div>
                 <div class="col-3">
                     <div class="box-vantaggi">
-                        <img src="images/icon-alugar.png" alt="">
-                        <p class="title">affitta</p>
-                        <p class="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean consectetur massa at commodo bibendum. Aliquam porta ornare purus vel posuere. Morbi porta sem quis tempor pretium. </p>
+                        <img src="images/icon-credito.svg" alt="Crédito">
+                        <p class="title">Acesso a Crédito</p>
+                        <p class="text">Amplie suas oportunidades de crescimento com acesso facilitado a linhas de crédito e serviços bancários específicos para Microempreendedores.</p>
                     </div>
                 </div>
             </div>
             <div class="row">
                 <div class="col-12">
-                    <a href="#" class="button-orange">Scopri i vantaggi</a>
+                    <a href="#" class="button-orange">Formalize-se agora!</a>
                 </div>
             </div>
         </div>
@@ -75,82 +108,89 @@
                 </div>
                 <div class="col">
                     <div class="box-call-action">
-                        <p class="title"><span>proprietario:</span> inserisci gratis il tuo annuncio</p>
-                        <p class="text">risparmia tempo e trova oggi
-                            la casa per te</p>
-                        <a href="#" class="button-orange btn-cta">Pubblica annuncio</a>
+                        <p class="title"><span>Conectando</span> clientes e profissionais</p>
+                        <p class="text">Economize tempo e encontre hoje mesmo o profissional MEI ideal para o serviço que você precisa</p>
+                        <a href="#" class="button-orange btn-cta">Encontrar profissional</a>
                     </div>
                 </div>
             </div>
+        </div>
     </section>
 
     <section class="rent">
         <div class="container">
             <div class="row">
+                <div class="col-12 text-center mb-5">
+                    <h2>SERVIÇOS POPULARES</h2>
+                </div>
+            </div>
+            <div class="row">
                 <div class="col-4">
                     <div class="card">
-                        <div class="status">disponibile subito</div>
-                        <!-- <div class="like liked"></div> -->
+                        <div class="status">Disponível</div>
 
                         <div class="card-body">
-                            <h5 class="type">Loft</h5>
-                            <p class="title">Bilocale Completamente Arredato</p>
-                            <p class="adress">Rua da Consolação, 3630</p>
-                            <ul class="list-itens">
-                                <li>68 m2</li>
-                                <li>2 Camere da letto</li>
-                                <li>Mezanino</li>
-                            </ul>
-                            <p class="mensal">Prezzo: 600€/mese</p>
+                            <h5 class="type">Construção Civil</h5>
+                            <p class="title">Jose Jaime</p>
+                            <p class="cnpj">CNPJ: 21.988/0001-00</p>
+                            <p class="adress">Zona Norte, São Paulo - SP</p>
+                            <p class="description"><strong>Descrição:</strong> Especialidade em construção, aplicação de azulejo, estrutural</p>
+                            <div class="contato">
+                                <p class="title-contato">Contato</p>
+                                <ul class="contato-info">
+                                    <li class="phone">45 99846-2423 <a href="https://wa.me/5545998462423" class="whatsapp-icon"><img src="images/whatsapp-icon.svg" alt="WhatsApp"></a></li>
+                                    <li class="email">
+                                        <a href="mailto:josejaime@email.com">josejaime@email.com</a>
+                                    </li>
+                                </ul>
+                            </div>
                         </div>
-                        <ul class="list-group list-group-flush">
-                            <li class="list-group-item">Totale: 750€/mese</li>
-                        </ul>
                     </div>
                 </div>
                 <div class="col-4">
                     <div class="card">
-                        <div class="status">disponibile subito</div>
-                        <!-- <div class="like unliked"></div> -->
+                        <div class="status">Disponível</div>
 
                         <div class="card-body">
-                            <h5 class="type">Loft</h5>
-                            <p class="title">Bilocale Completamente Arredato</p>
-                            <p class="adress">Rua da Consolação, 3630</p>
-                            <ul class="list-itens">
-                                <li>68 m2</li>
-                                <li>2 Camere da letto</li>
-                                <li>Mezanino</li>
-                            </ul>
-                            <p class="mensal">Prezzo: 600€/mese</p>
+                            <h5 class="type">Construção Civil</h5>
+                            <p class="title">Jose Jaime</p>
+                            <p class="cnpj">CNPJ: 21.988/0001-00</p>
+                            <p class="adress">Zona Oeste, São Paulo - SP</p>
+                            <p class="description"><strong>Descrição:</strong> Especialidade em construção, aplicação de azulejo, estrutural</p>
+                            <div class="contato">
+                                <p class="title-contato">Contato</p>
+                                <ul class="contato-info">
+                                    <li class="phone">45 99846-2423 <a href="https://wa.me/5545998462423" class="whatsapp-icon"><img src="images/whatsapp-icon.svg" alt="WhatsApp"></a></li>
+                                    <li class="email">
+                                        <a href="mailto:josejaime@email.com">josejaime@email.com</a>
+                                    </li>
+                                </ul>
+                            </div>
                         </div>
-                        <ul class="list-group list-group-flush">
-                            <li class="list-group-item">Totale: 750€/mese</li>
-                        </ul>
                     </div>
                 </div>
                 <div class="col-4">
                     <div class="card">
-                        <div class="status">disponibile subito</div>
-                        <!-- <div class="like unliked"></div> -->
+                        <div class="status">Disponível</div>
 
                         <div class="card-body">
-                            <h5 class="type">Loft</h5>
-                            <p class="title">Bilocale Completamente Arredato</p>
-                            <p class="adress">Rua da Consolação, 3630</p>
-                            <ul class="list-itens">
-                                <li>68 m2</li>
-                                <li>2 Camere da letto</li>
-                                <li>Mezanino</li>
-                            </ul>
-                            <p class="mensal">Prezzo: 600€/mese</p>
+                            <h5 class="type">Construção Civil</h5>
+                            <p class="title">Jose Jaime</p>
+                            <p class="cnpj">CNPJ: 21.988/0001-00</p>
+                            <p class="adress">Zona Sul, São Paulo - SP</p>
+                            <p class="description"><strong>Descrição:</strong> Especialidade em construção, aplicação de azulejo, estrutural</p>
+                            <div class="contato">
+                                <p class="title-contato">Contato</p>
+                                <ul class="contato-info">
+                                    <li class="phone">45 99846-2423 <a href="https://wa.me/5545998462423" class="whatsapp-icon"><img src="images/whatsapp-icon.svg" alt="WhatsApp"></a></li>
+                                    <li class="email">
+                                        <a href="mailto:josejaime@email.com">josejaime@email.com</a>
+                                    </li>
+                                </ul>
+                            </div>
                         </div>
-                        <ul class="list-group list-group-flush">
-                            <li class="list-group-item">Totale: 750€/mese</li>
-                        </ul>
                     </div>
                 </div>
-
             </div>
         </div>
     </section>
